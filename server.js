0d05ddeb81c198642d0de0535e22e1b48c65fa4b
mr-rupak007 <rupakpatra47@gmail.com>
2026-05-03 18:46:51 +0530

    fix frontend serving

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.use(
 );
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "*",
+  origin: process.env.FRONTEND_URL,
   credentials: true
 }));
 
@@ -88,7 +88,7 @@ app.use(express.static(path.join(__dirname, "../frontend")));
 
 // ================= ROOT =================
 app.get("/", (req, res) => {
-  res.send("Backend running 🚀");
+  res.sendFile(path.join(__dirname, "../frontend/index.html"));
 });
 
 // ================= FALLBACK (VERY IMPORTANT - LAST) =================
